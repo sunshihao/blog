5417a3e1c0567b9c612f526acf480dbb6574bd1e
snailrun <3074994545@qq.com>
2024-08-06 22:33:08 +0800

    feat: 添加百度统计和谷歌分析

---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,27 @@
+'use client';
+import Script from 'next/script';
+const GoogleAnalytics = () => {
+	return (
+		<>
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ID}`}
+			/>
+			<Script
+				id="gtag-init"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ID}', {
+            page_path: window.location.pathname,
+            });
+          `
+				}}
+			/>
+		</>
+	);
+};
+export default GoogleAnalytics;
