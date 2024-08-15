e8f2837741d5e40451e53567d2491ed865ee4d41
coderPerseus <snailrun160@gmail.com>
2024-08-15 23:51:31 +0800

    fix: 移动端优化

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -63,7 +63,7 @@ export function Footer() {
 					<Link
 						target="_blank"
 						href="https://beian.miit.gov.cn/"
-						className="absolute bottom-6 left-1/2 -translate-x-1/2 flex justify-center items-center"
+						className="absolute w-full bottom-6 left-1/2 -translate-x-1/2 flex justify-center items-center"
 					>
 						<Image
 							unoptimized
