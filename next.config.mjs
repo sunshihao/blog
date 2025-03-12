a7ef07a7cdf9c0aec7da9ab8d173eee39ce79625
Sunny Lee <sunshihao945@outlook.com>
2025-03-12 17:28:23 +0800

    feat: 触发

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import generate from './scripts/generate-rss.js';
 const nextConfig = {
 	// 静态导出
 	output: 'export',
-	basePath: '/blog',
+	basePath: '/docs',
 	reactStrictMode: true,
 	swcMinify: true,
 	// images: {
