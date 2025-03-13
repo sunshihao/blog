a914ea3c4b69702152a7d50866b3a27df6131f6b
Sunny Lee <sunshihao945@outlook.com>
2025-03-13 12:02:40 +0800

    fix: 调整

---
+++ docs/static/weVExY6-jENQW9lG_RHEX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-591cd42735fea7c8.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
